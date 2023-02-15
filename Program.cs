4620aec088e81e7aeb87aab60387b4d17c03683c
Sergey Drepin <sdrepin@mail.ru>
2023-02-15 10:14:56 +0300

    Окончательный вариант

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ double i = 2;
 Console.WriteLine("Введите число n");
 n = Convert.ToDouble(Console.ReadLine());
 
-for (i < n, i += 2);
+while (i <= n)
+{
+    if (i % 2 == 0)
+        {
+        Console.WriteLine(i);
+        }
 
-Console.WriteLine(i);
+    i = i + 1;
+}
+
